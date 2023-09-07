116f188acb7b288407217ea8f5ed93a6bc1265c1
FelipeDias0058 <143054608+FelipeDias0058@users.noreply.github.com>
2023-09-07 00:25:13 -0300

    1_funcao_calculo.py

--- 1_funcao_calculo.py
+++ 1_funcao_calculo.py
@@ -1,12 +1,16 @@
+#Entrada de Dados
 a = float(input(""))
 b = float(input(""))
 c = float(input(""))
 
 
+#Função para o cálculo da expressão numérica
 def calcular (a, b, c):
     return (2 * a) + (5 * b) - c
 
 
+#Chamada da função "calcular"
 r = calcular(a, b, c)
 
+#Exibição do resultado da expressão
 print(r)
