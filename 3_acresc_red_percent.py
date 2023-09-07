3958f6efa983abff3dd53cc47a7e8eb8b686bc18
FelipeDias0058 <143054608+FelipeDias0058@users.noreply.github.com>
2023-09-07 01:32:11 -0300

    3_acresc_red_percent.py

--- 3_acresc_red_percent.py
+++ 3_acresc_red_percent.py
@@ -1,11 +1,25 @@
+#Funções para cálculo de porcentagem, acréscimo e desconto
+def percent(valor, valor_percent):
+    porcentagem = valor * valor_percent / 100
+    return porcentagem
+
+def acr(valor, valor_percent):
+    acrescimo = valor + porcentagem
+    return acrescimo
+
+def desc(valor, valor_percent):
+    desconto = valor - porcentagem
+    return desconto
+
+#Entrada de dados
 valor = float(input(""))
-porcentagem = float(input(""))
+valor_percent = float(input(""))
 
-acrescimo = valor * porcentagem / 100
-v_acrescimo = valor + acrescimo
+#Chamada das Funções
+porcentagem = percent(valor, valor_percent)
+acrescimo = acr(valor, valor_percent)
+desconto = desc(valor, valor_percent)
 
-desconto = valor * porcentagem / 100
-v_desconto = valor - desconto
-
-print(f'{v_acrescimo:.2f}')
-print(f'{v_desconto:.2f}')
+#Exibição do acréscimo e desconto
+print(f'{acrescimo:.2f}')
+print(f'{desconto:.2f}')
