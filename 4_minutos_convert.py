8783d5dcb61d9f8b71df08d73a2d3d474133064b
FelipeDias0058 <143054608+FelipeDias0058@users.noreply.github.com>
2023-09-07 00:42:45 -0300

    4_minutos_convert.py

--- 4_minutos_convert.py
+++ 4_minutos_convert.py
@@ -1,4 +1,14 @@
+#Função para converter os minutos em horas e minutos
+def conversao(x):
+    horas = x // 60
+    minutos = x % 60
+    return horas, minutos
+
+#Entrada dos minutos a serem convertidos
 x = int(input(""))
-horas = x // 60
-minutos = x % 60
+
+#Atribui horas/minutos calculados às respectivas variáveis
+horas, minutos = conversao(x)
+
+#Mostra na tela as horas e minutos
 print(f'{horas}:{minutos}')
